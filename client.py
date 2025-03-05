7cf9140b81c71e731145e3e7904f18e7ca750a58
vlad113chepurko <vlad113chepurko@gmail.com>
2025-03-05 12:43:45 +0200

    Added function, send data, interface

--- client.py
+++ client.py
@@ -1,7 +1,9 @@
 import socket
 from tkinter import *
-from tkinter import tkk
+from tkinter import ttk
+
 root = Tk()
+root.geometry("400x400")
 root.title("Calculator")
 
 PATH = "127.0.0.1"
@@ -10,5 +12,38 @@ PORT = 12345
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((PATH, PORT))
 
+# main code
+
+operations = []
+
+def calculate():
+    try:
+      expr = input.get()
+      result = eval(expr)
+      string = f"{expr} = {result}"
+      operations.append(string)
+      label_exp = Label(root, text=string, background="lightgrey")
+      label_exp.pack()
+    except:
+       print("Error")
+
+def send():
+    client.send(str(operations).encode())
+    print("Data has been sent to the server!")
+
+greeting = Label(root, text="Welcome to the calculator!")
+greeting.pack()
+
+lable_enter = Label(root, text="Enter the expression:")
+lable_enter.pack()
+
+input = Entry(root, width=50, background="lightgrey")
+input.pack()
+
+send_btn = Button(root, text="Send", background="lightblue", command=calculate)
+send_btn.pack()
+
+send_info_to_server = Button(root, text="Send info to server", background="lightgreen", command=send)
+send_info_to_server.pack()
 
 root.mainloop()
--- server.py
+++ server.py
@@ -1,12 +1,23 @@
 import socket
 
-PATH = "127.0.0.1"
+HOST = "127.0.0.1"
 PORT = 12345
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((PATH, PORT))
-
+server.bind((HOST, PORT))
 server.listen(1)
+
 print("Server is listening...")
-client, address = server.accept()
-print(f"Connection from {address} has been established!")
+
+while True:
+    client_socket, addr = server.accept()
+
+    data = client_socket.recv(1024).decode()  
+    if not data:
+        break
+
+    print("Received data:", data)
+
+    client_socket.close()  
+
+server.close()
