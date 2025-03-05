f352e9c644572cd85bb6c63b575b1492abacf2f4
vlad113chepurko <vlad113chepurko@gmail.com>
2025-03-05 13:32:08 +0200

    Added database

--- server.py
+++ server.py
@@ -1,4 +1,12 @@
 import socket
+import pyodbc
+
+server = 'localhost'
+database = 'Test'
+username = 'vlad'
+password = 'admin'
+
+dsn = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password};Trusted_Connection=yes;'
 
 HOST = "127.0.0.1"
 PORT = 12345
@@ -9,15 +17,45 @@ server.listen(1)
 
 print("Server is listening...")
 
-while True:
-    client_socket, addr = server.accept()
+connect, address = server.accept()
+print(f"Connection from {address} has been established!")
 
-    data = client_socket.recv(1024).decode()  
-    if not data:
+while True:
+    try:
+        data = connect.recv(1024).decode()
+        if not data:
+            print("No data received. Closing connection...")
+            break
+
+        print(f"Data from the client: {data}")
+
+        try:
+            conn = pyodbc.connect(dsn)
+            cursor = conn.cursor()
+
+            cursor.execute("SELECT TOP 10 * FROM Calculate")
+            rows = cursor.fetchall()
+
+            print("Data from the database:")
+            for row in rows:
+                print(row)
+
+            insert_query = "INSERT INTO Calculate ([Result]) VALUES (?)"
+            values = (data)
+            cursor.execute(insert_query, values)
+            conn.commit()
+
+            print("Data has been inserted successfully!")
+
+            cursor.close()
+            conn.close()
+        except Exception as e:
+            print(f"Database error: {e}")
+            break
+    except Exception as e:
+        print(f"error: {e}")
         break
 
-    print("Received data:", data)
-
-    client_socket.close()  
-
+connect.close()
 server.close()
+print("Server has been closed.")
